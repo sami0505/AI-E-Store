2d4ec99a3a3fe3370a0d27c40090754e0ab1eece
sami0505 <sami0505@abv.bg>
2023-01-29 12:05:45 +0000

    Added all unit 1.1 tests

--- AI_E_Store/customerSection/tests.py
+++ AI_E_Store/customerSection/tests.py
@@ -4,8 +4,8 @@ import datetime
 import unittest
 # Create your tests here.
 
-class databaseTests(TestCase):
-    def test1_1_1(self):
+class unit_1_1_Tests(TestCase):
+    def test_1_1_1(self):
         """ This is test 1.1.1. """
         try:
             newCustomer = Customer()
@@ -22,7 +22,7 @@ class databaseTests(TestCase):
         except Exception as error:
             self.fail(f"The test failed because an error occured: {error}")
 
-    def test1_1_2(self):
+    def test_1_1_2(self):
         """ This is test 1.1.2 """
         try:
             newCustomer = Customer()
@@ -39,7 +39,7 @@ class databaseTests(TestCase):
         except Exception as error:
             self.fail(f"The test failed because an error occured: {error}")
 
-    def test1_1_3(self):
+    def test_1_1_3(self):
         """ This is test 1.1.3 """
         with self.assertRaises(Exception):
             newCustomer = Customer()
@@ -52,5 +52,44 @@ class databaseTests(TestCase):
             newCustomer.Title = "Large"
             newCustomer.DateOfBirth = datetime.date(1970, 1, 1)
             newCustomer.DateJoined = datetime.date.today()
+            newCustomer.save()
+            self.fail(f"The test failed because an error should have occured!")
+    
+    def test_1_1_4(self):
+        """ This is test 1.1.4 """
+        with self.assertRaises(Exception):
+            newCustomer = Customer()
+            newCustomer.Firstname = "John"
+            newCustomer.Surname = "Doe"
+            newCustomer.Email = "foo.bar@doemail.org"
+            newCustomer.Telephone = "05555555555"
+            newCustomer.Username = "johnDoe.01"
+            newCustomer.Password = "1@2B3c4."
+            newCustomer.Title = "Mr"
+            newCustomer.DateOfBirth = datetime.date(0, 1, 1)
+            newCustomer.DateJoined = datetime.date(0, 1, 1)
+            newCustomer.save()
+            self.fail(f"The test failed because an error should have occured!")
+    
+    def test_1_1_5(self):
+        """ This is test 1.1.5 """
+        with self.assertRaises(Exception):
+            newCustomer = Customer()
+            newCustomer.save()
+            self.fail(f"The test failed because an error should have occured!")
+        
+    def test_1_1_6(self):
+        """ This is test 1.1.6"""
+        with self.assertRaises(Exception):
+            newCustomer = Customer()
+            newCustomer.Firstname = 1
+            newCustomer.Surname = 2
+            newCustomer.Email = 3
+            newCustomer.Telephone = 4
+            newCustomer.Username = 5
+            newCustomer.Password = 6
+            newCustomer.Title = 7
+            newCustomer.DateOfBirth = 8
+            newCustomer.DateJoined = 9
             newCustomer.save()
             self.fail(f"The test failed because an error should have occured!")

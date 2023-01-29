7e0df41595485cb4e697d7e065613e156388528d
sami0505 <sami0505@abv.bg>
2023-01-29 00:29:32 +0000

    Added Review and Item models
    Also fixed bug where migration wouldn't occur due to a pre-instated primary key.

--- AI_E_Store/customerSection/models.py
+++ AI_E_Store/customerSection/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
+    CustomerID = models.IntegerField(primary_key=True)
     Firstname = models.CharField(max_length=32)
     Surname = models.CharField(max_length=32)
     Email = models.EmailField(max_length=64)
@@ -13,3 +14,17 @@ class Customer(models.Model):
     DateOfBirth = models.DateField()
     DateJoined = models.DateField()
     IsActivated = models.BooleanField()
+
+class Item(models.Model):
+    ItemID = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=32)
+    Description = models.CharField(max_length=256)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Category = models.CharField(max_length=20)
+
+class Review(models.Model):
+    ReviewID = models.IntegerField(primary_key=True)
+    CustomerID = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    ItemID = models.ForeignKey('Item', on_delete=models.CASCADE)
+    Comment = models.CharField(max_length=256)
+    StarRating = models.IntegerField()

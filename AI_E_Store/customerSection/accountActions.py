19b24551ee46b5d239d1c8cfabbbcb43141ab309
sami0505 <sami0505@abv.bg>
2023-01-31 19:19:21 +0000

    Added Register Form and empty register()

---
+++ AI_E_Store/customerSection/accountActions.py
@@ -0,0 +1,8 @@
+def registerAccount():
+    status = True
+    try:
+        pass
+    except:
+        pass
+    finally:
+        pass
--- AI_E_Store/customerSection/forms.py
+++ AI_E_Store/customerSection/forms.py
@@ -1,4 +1,14 @@
 from django import forms
 
 class Register(forms.Form):
-    Firstname = forms.CharField(max_length=100)
+    titleChoices = ["Mr", "Mrs", "Ms", "Lord", "Large"] # Large is INVALID input
+    # This results in a dropdown instead of an input box
+    Title = forms.CharField(label="Title:", widget = forms.Select(choices=titleChoices) )
+    
+    Firstname = forms.CharField(max_length=32)
+    Surname = forms.CharField(max_length=32)
+    Email = forms.CharField(max_length=64)
+    Telephone = forms.CharField(max_length=11)
+    Username = forms.CharField(max_length=16)
+    Password = forms.CharField(max_length=60)
+    DateOfBirth = forms.DateField()
